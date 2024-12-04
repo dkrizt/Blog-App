41a228110a69fa07a07816b7e530ae649dbea95b
dkrizt <donkrizt@gmail.com>
2024-12-04 21:03:55 +0100

    Alter viteconfig to remove base for netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Blog-App/', // Add base path for GitHub Pages
+  // base: '/Blog-App/', // Add base path for GitHub Pages
   plugins: [react()],
 });
